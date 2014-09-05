84b33e66c0bb0bc95cb222afd138b3eacc2716de
Tomasz Mieszkowski <tomasz.mieszkowski@ext.allegro.pl>
2014-09-05 11:32:08 +0200

    Two small fixes for the 'advanced_plot' tile.

--- tipboard/tiles/advanced_plot.js
+++ tipboard/tiles/advanced_plot.js
@@ -28,7 +28,7 @@
         var renderersSwapper = new RenderersSwapper();
         renderersSwapper.swap(newMeta);
         AdvancedPlotTile.rescaleContainers(tile);
-        AdvancedPlotTile.buildChart(tileId, data.plotData, newMeta);
+        AdvancedPlotTile.buildChart(tileId, data.plot_data, newMeta);
     }
 
     Tipboard.Dashboard.registerUpdateFunction('advanced_plot', updateTileAdvancedPlot);
