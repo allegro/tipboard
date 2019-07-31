13af01f0ccde99bb27492008c16db3e43ad6d374
MAXIME RENAUD <maxime.renaud@epitech.eu>
2019-07-31 14:24:54 +0200

    features(Django-template) update urls.py and settings.py

--- webserver/settings.py
+++ webserver/settings.py
@@ -102,7 +102,6 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             'hosts': [(REDIS_HOST, REDIS_PORT)],
-            'prefix': "lkfjezojezfezfj"
         },
     }
 }
--- webserver/urls.py
+++ webserver/urls.py
@@ -1,13 +1,23 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView
 from django.conf.urls import url
 from tipboard.properties import *
+from tipboard.views.api import push, projectInfo, deleteTile, tile, getTile, meta
+from tipboard.views.dashboard import dashboardRendererHandler, getDashboardsPaths, flipboardHandler
 
 
 favicon_view = RedirectView.as_view(url="/static/" + 'favicon.ico', permanent=True)
 
 urlpatterns = [
-
+    url(r'^flipboard/getDashboardsPaths$', getDashboardsPaths),
+    url(r'^$', flipboardHandler),
+    url(r'^([a-zA-Z0-9_-]*)$', dashboardRendererHandler),
+    url(r'^api/' + API_VERSION + '/' + API_KEY + '/tiledata/([a-zA-Z0-9_-]+)$', tile),
+    url(r'^api/' + API_VERSION + '/' + API_KEY + '/tileconfig/([a-zA-Z0-9_-]+)$', meta),
+    url(r'^api/' + API_VERSION + '/' + API_KEY + '/info$', projectInfo),
+    url(r'^api/' + API_VERSION + '/' + API_KEY + '/push$', push),
+    url(r'^tile/(\w+)/push/$', push),
+    #url(r'^$', dashboard)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
