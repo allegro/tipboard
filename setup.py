3ae761abf199138f72445831fbb6d63ba4db518c
MAXIME RENAUD <maxime.renaud@epitech.eu>
2019-08-12 20:15:13 +0200

    fix(setup.py) im an idiot

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=required,
     entry_points={
         'console_scripts': [
-            'tipboard = src.manage:bypassme',
+            'tipboard = src.manage:main_as_pkg',
         ],
     },
 )
