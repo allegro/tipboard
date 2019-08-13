68fa498c803b4cdaab00e52ea0113dbeb66cf35b
MAXIME RENAUD <maxime.renaud@epitech.eu>
2019-08-13 12:56:46 +0200

    fix(version)

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 
-
-VERSION = (2, 0, 0)
+VERSION = (2, 0, 6)
 __version__ = '.'.join(str(num) for num in VERSION)
