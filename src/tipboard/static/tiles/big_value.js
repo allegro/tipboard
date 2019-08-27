7fd2746ca152d1aff09a61148720af16389fbe7a
MAXIME RENAUD <maxime.renaud@epitech.eu>
2019-08-27 19:36:22 +0200

    features(ChartJS) fix fancy listing

--- src/tipboard/static/tiles/big_value.js
+++ src/tipboard/static/tiles/big_value.js
@@ -1,7 +1,6 @@
 /*jslint browser: true, devel: true*/
 /*global WebSocket: false, Tipboard: false*/
 
-
 function updateTileBigValue(tileId, data, config) {
     var tile = Tipboard.Dashboard.id2node(tileId);
     BigValueTile.setDataByKeys(tileId, data, 'all');
@@ -22,9 +21,9 @@ BigValueTile = {
         // DEPRECATED function, Tipboard.DisplayUtils.applyHighlighterConfig
         var color = meta['big_value_color'];
         if (typeof(color) !== 'undefined') {
-            color = Tipboard.DisplayUtils.replaceFromPalette(color)
+            color = Tipboard.DisplayUtils.replaceFromPalette(color);
             var tile = Tipboard.Dashboard.id2node(tileId);
-            var bigValue = $(tile).find('#' + tileId + '-big-value')[0]
+            var bigValue = $(tile).find('#' + tileId + '-big-value')[0];
             var dst = $(bigValue).parent();
             $(dst).css('background-color', color);
         }
