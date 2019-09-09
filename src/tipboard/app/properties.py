9d5a3da9929bf548d81ae518a968f9f5e092cc8f
MAXIME RENAUD <maxime.renaud@epitech.eu>
2019-09-09 19:50:09 +0200

    merging(ChartJS) upgrading all chart name

--- src/tipboard/app/properties.py
+++ src/tipboard/app/properties.py
@@ -33,10 +33,8 @@ REDIS_PASSWORD = conf['REDIS_PASSWORD']
 REDIS_DB = conf['REDIS_DB']
 
 ALLOWED_TILES = ["text", "fancy_listing", "simple_percentage", "listing", "big_value", "just_value", #Homemade
-                 "norm_chart", "pie_chart", "line_chart", "cumulative_flow", "advanced_plot", "bar_chart", #jqplot lib
                  "empty",
-                 "norm_chartjs", "pie_chartjs", "line_chartjs",
-                 "cumulative_flowjs","advanced_plotjs", "bar_chartjs"] #chartjs lib
+                 "norm_chart", "pie_chart", "line_chart", "cumulative_flow","advanced_plot", "bar_chart"] #chartjs lib
 
 
 TIPBOARD_CSS_STYLES = [
--- src/tipboard/app/views/dashboard.py
+++ src/tipboard/app/views/dashboard.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from django.http import JsonResponse, HttpResponse
 from django.contrib.staticfiles import finders
 from django.shortcuts import render
@@ -20,7 +19,6 @@ def flipboardHandler(request):
         "tipboard_js": ['js/flipboard.js'],
         "flipboard_interval": FLIPBOARD_INTERVAL,
     }
-#    data['tipboard_js'].remove('js/tipboard.js')
     print(f"{getTimeStr()} (+) flipboardHandler/ -> CSS: {data['tipboard_css']}", flush=True)
     return render(request, 'flipboard.html', data)
 
@@ -37,26 +35,21 @@ def dashboardRendererHandler(request, layout_name='layout_config'):
         print(f"{getTimeStr()} GET dashboardRendererHandler /{layout_name}", flush=True)
     try:
         config = process_layout_config(layout_name)
-        tiles_js = ["tiles/" + '.'.join((name, 'js')) for name in config['tiles_names']]
-        tiles_css = ["tiles/" + '.'.join((name, 'css')) for name in config['tiles_names']]
 
         data = {
             "details": config['details'],
             "layout": config['layout'],
             "tipboard_css": TIPBOARD_CSS_STYLES,
             "tipboard_js": TIPBOARD_JAVASCRIPTS,
-            "tiles_css": tiles_css,
-            "tiles_js": tiles_js,
+            "tiles_css": ["tiles/" + '.'.join((name, 'css')) for name in config['tiles_names']],
+            "tiles_js": ["tiles/" + '.'.join((name, 'js')) for name in config['tiles_names']],
         }
     except FileNotFoundError as e:
         if LOG:
             print(f"{getTimeStr()}: (+)Config file:{layout_name} not found", flush=True)
-        msg = '<br>'.join([
-            '<div style="color: red">',
-            f'No config file found for dashboard: {layout_name}',
-            f'Make sure that file: "{e.filename}" exists.',
-            '</div>',
-        ])
+        msg = f'<br> <div style="color: red"> ' \
+            f'No config file found for dashboard: {layout_name} ' \
+            f'Make sure that file: "{e.filename}" exists. </div>'
         return HttpResponse(msg, status=404)
     tiles_css = list()
     for tile_css in data['tiles_css']:
@@ -64,5 +57,3 @@ def dashboardRendererHandler(request, layout_name='layout_config'):
             tiles_css.append(tile_css)
     data['tiles_css'] = tiles_css
     return render(request, 'layout.html', data)
-
-
