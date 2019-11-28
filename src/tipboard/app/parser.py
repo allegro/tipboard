d0cd68668df2f5aef734edd221c56af71c24eb4a
max <maxime.renaud@epitech.eu>
2019-11-28 22:18:05 +0100

    features(parser) python for codacy

--- src/tipboard/app/parser.py
+++ src/tipboard/app/parser.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 import glob, os, yaml
-
 from src.tipboard.app import properties
-
+from src.tipboard.app.utils import getTimeStr
 
 class WrongSumOfRows(Exception):
     pass
@@ -85,21 +83,24 @@ def get_tiles_configs():
     return tiles_configs
 
 
-def find_tiles_names(layout):
-    name_list, key_list = [], []
-    for row in get_rows(layout):
-        for col in get_cols(row):
-            for tile_dict in list(col.values())[0]:
-                name = tile_dict['tile_template']
-                key = tile_dict['tile_id']
-                if key not in key_list:
-                    key_list.append(key)
-                    name_list.append(name)
-    return name_list, key_list
+def analyse_cols(tiles_dict, tiles_id, tiles_templates):
+    for tile_dict in tiles_dict:
+        if tile_dict['tile_id'] not in tiles_id:
+            tiles_id.append(tile_dict['tile_id'])
+            tiles_templates.append(tile_dict['tile_template'])
+
+
+def find_tiles_names(cols_data):
+    tiles_templates, tiles_id = list(), list()
+    for col_dict in cols_data:
+        for col_name, tiles_dict in col_dict.items():
+            analyse_cols(tiles_dict, tiles_id, tiles_templates)
+    return tiles_templates, tiles_id
 
 
 def parse_xml_layout(layout_name='layout_config'):
     config_path = config_file_name2path(layout_name)
+    config = dict()
     try:
         with open(config_path, 'r') as layout_config:
             config = yaml.safe_load(layout_config)
@@ -108,9 +109,12 @@ def parse_xml_layout(layout_name='layout_config'):
             config_path += ".yaml"
         with open(config_path, 'r') as layout_config:
             config = yaml.safe_load(layout_config)
-    layout = config['layout']
-    config['tiles_names'], config['tiles_keys'] = find_tiles_names(layout)
-    return config
+    finally:
+        rows = [row for row in get_rows(config['layout'])]
+        cols = [col for col in [get_cols(row) for row in rows]]
+        cols_data = [colsValue for colsList in cols for colsValue in colsList]
+        config['tiles_names'], config['tiles_keys'] = find_tiles_names(cols_data)
+        return config
 
 
 def getSrcJssForTilesInLayout(tiles_name):
