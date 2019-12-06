a11ea5fbc7da82dfe40940c97dea43a13aad4549
max <maxime.renaud@epitech.eu>
2019-12-06 22:07:59 +0100

    features(quality) js for codacy, finaly ?

--- src/tipboard/static/tiles/text.js
+++ src/tipboard/static/tiles/text.js
@@ -1,18 +1,25 @@
 /**
  * Update text tile, font, color & value
- * @param id
+ * @param tileId
  * @param data
  * @param meta
  * @param tileType
  */
 function updateTileText(tileId, data, meta, tileType) {
     console.log("updateTileText::updateTile::start" + tileId);
-    let title = document.getElementById(tileId + '-title');
-    let body = document.getElementById(tileId + '-body');
-    if ("title" in data) {
-        title.innerHTML = data["title"];
+    let parser = new DOMParser();
+//    let title = document.getElementById(tileId + "-title");
+    let body = document.getElementById(tileId + "-body");
+//    if ("title" in data) {
+//        title.innerHTML = data["title"];
+//    }
+    let parsed = parser.parseFromString(data["text"], `text/html`);
+    let tags = parsed.getElementsByTagName(`body`);
+    body.innerHTML = '';
+    for (const tag of tags) {
+        body.appendChild(tag);
     }
-    body.innerHTML = data["text"];
+
 }
 
-Tipboard.Dashboard.registerUpdateFunction('text', updateTileText);
+Tipboard.Dashboard.registerUpdateFunction("text", updateTileText);
