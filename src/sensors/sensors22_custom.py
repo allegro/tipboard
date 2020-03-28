996d79876d0e77668f545ca7dc47ae8db3dcede8
max <maxime.renaud@epitech.eu>
2020-03-28 18:49:41 +0100

    fix(custom_tile) add real exemple
    Signed-off-by: max <maxime.renaud@epitech.eu>

--- src/sensors/sensors22_custom.py
+++ src/sensors/sensors22_custom.py
@@ -2,27 +2,31 @@ import time
 from src.sensors.utils import end, sendUpdateByApi
 
 
-def returnHttPage():
+def returnHttPageExemple():
+    """ Basic Html exemple with css """
     return '''
-    <link property rel="stylesheet" href="http://snippi.com/raw/j5v0t66" media="screen" type="text/html"/>
-    <div class="grid">
-        <img src="https://dummyimage.com/824x600/1FD0C0/fff&text=%20" class="l" id="coco" alt="app">
-        <img src="https://dummyimage.com/400x600/FF5046/fff&text=%20" class="m" id="cocote" alt="affiche">
-        <img src="https://dummyimage.com/400x288/FFDE13/fff&text=%20" class="s" alt="site">
-        <img src="https://dummyimage.com/824x600/1FD0C0/fff&text=%20" class="l" alt="app">
-        <img src="https://dummyimage.com/400x600/FF5046/fff&text=%20" class="m" alt="affiche">
-        <img src="https://dummyimage.com/400x288/FFDE13/fff&text=%20" class="s" alt="site">
-        <img src="https://dummyimage.com/824x600/1FD0C0/fff&text=%20" class="l" alt="app">
-        <img src="https://dummyimage.com/400x600/FF5046/fff&text=%20" class="m" alt="affiche">
-        <img src="https://dummyimage.com/400x288/FFDE13/fff&text=%20" class="s" alt="site">
+    <div class="contener" id="mycontener">
+         <div class="card">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0MrhIzjan2GfDeqNVqyIDjBXqP7J7GwDJxd_OKwp91Lj4JS4t">
+             <div class="container">
+                  <h4><b>Mister cat</b></h4>
+                  <p>Look in my eyes darling</p>
+             </div>
+        </div> 
     </div>
+    <style>
+    #mycontener {
+        display: flex;
+        justify-content: center;     
+    }
+    </style>
     '''
 
 
 def sonde22(tester=None, tile_id='custom_ex'):
     start_time = time.time()
     data = {
-        'text': returnHttPage()
+        'text': returnHttPageExemple()
     }
     tipboardAnswer = sendUpdateByApi(data=data, tileTemplate='custom', tileId=tile_id, tester=tester)
     end(title=f'sensors custom tile -> {tile_id}', startTime=start_time, tipboardAnswer=tipboardAnswer, tileId=tile_id)
