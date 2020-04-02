ea5dfa2222228e289e36faa922da47be6d3ac000
max <maxime.renaud@epitech.eu>
2020-04-03 01:24:47 +0200

    features(white-noise) enabling production mode
    Signed-off-by: max <maxime.renaud@epitech.eu>

--- src/manage.py
+++ src/manage.py
@@ -31,6 +31,6 @@ if __name__ == '__main__':
     if argv in ('sensors', '-s'):
         from src.sensors.sensors_main import scheduleYourSensors
         scheduleYourSensors(BlockingScheduler())
-    elif argv in ('test', 'runserver', 'migrate', 'shell'):
+    elif argv in ('test', 'runserver', 'migrate', 'shell', 'collectstatic', 'findstatic'):
         exit(startDjango())
     exit(show_help())
